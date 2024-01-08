29d7e556e902ce4e9768f7b090566b3669ba5334
Atul Mahankal <atulmahankal@gmail.com>
2024-01-08 18:29:30 +0530

    changes

--- config/express.js
+++ config/express.js
@@ -4,3 +4,26 @@ module.exports = {
     '127.0.0.1'
   ]
 }
+
+module.exports.staticConfig = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html', 'css', 'js', 'ico', 'jpg', 'jpeg', 'png', 'svg'],
+  index: ['index.html'],
+  maxAge: '1m',
+  redirect: false
+}
+
+module.exports.paths = [
+  {
+    url: "downloads",
+  },
+  {
+    url: "images",
+    options: {
+      dotfiles: 'ignore',
+      etag: false,
+      extensions: ['ico', 'jpg', 'jpeg', 'png', 'svg'],
+    }
+  }
+]
