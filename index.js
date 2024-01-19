03ba1fc56444445f5d08cd96d1383b8dd09100fe
Atul Mahankal <atulmahankal@gmail.com>
2024-01-19 20:47:16 +0530

    update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { Server, config } = require('./lib/server.js');
 const server = new Server();
-const app = require('express')();
+// const app = require('express')();
 
-app.get('/', (req, res) => {
-  res.send(`Welcome to myapp!`);
-})
+// app.get('/', (req, res) => {
+//   res.send(`Welcome to myapp!`);
+// })
 
-server.use(app);
+// server.use(app);
 
 server.on('started', () => {
   console.log();
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+router.get('/', (req, res) => {
+	res.send(`<h1>Welcome to ExpressBooster!</h1>`);
+});
+
 router.get('/home',(req, res)=>{
 	res.send('Welcome to home!');
 })
